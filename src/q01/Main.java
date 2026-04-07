6a4fc71c8fe8bd1c90f358fd36feb6c611a4b707
Sachin Nishad <154582625+Sachin-Nishad@users.noreply.github.com>
2026-04-07 11:25:28 +0530

    Update Main.java

--- src/q01/Main.java
+++ src/q01/Main.java
@@ -1,24 +1,23 @@
 import java.util.*;
 
-class Main {
+public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
 
-        // Upper part
-        for (int i = 0; i < n; i++) {
-
-            // Leading spaces
-            if (i % 2 == 1) {
+       
+        for (int i = 1; i <= n; i++) {
+            
+            for (int j = 1; j <= n - i; j++) {
                 System.out.print(" ");
             }
 
-            // First star
+           
             System.out.print("*");
 
-            // Hollow part
-            if (i > 0) {
-                for (int j = 0; j < 2 * i - 1; j++) {
+            
+            if (i > 1) {
+                for (int j = 1; j <= 2 * i - 3; j++) {
                     System.out.print(" ");
                 }
                 System.out.print("*");
@@ -27,18 +26,19 @@ class Main {
             System.out.println();
         }
 
-        // Lower part
-        for (int i = n - 2; i >= 0; i--) {
-
-            // Leading spaces
-            if (i % 2 == 1) {
+      
+        for (int i = n - 1; i >= 1; i--) {
+           
+            for (int j = 1; j <= n - i; j++) {
                 System.out.print(" ");
             }
 
+          
             System.out.print("*");
 
-            if (i > 0) {
-                for (int j = 0; j < 2 * i - 1; j++) {
+            
+            if (i > 1) {
+                for (int j = 1; j <= 2 * i - 3; j++) {
                     System.out.print(" ");
                 }
                 System.out.print("*");
