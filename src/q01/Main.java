18922f8a034974f0ef1ce0c4fe62f1a8db5d4954
Sachin Nishad <154582625+Sachin-Nishad@users.noreply.github.com>
2026-04-07 10:43:23 +0530

    Update Main.java

--- src/q01/Main.java
+++ src/q01/Main.java
@@ -1,9 +1,46 @@
 import java.util.*;
 
-public class Main {
+class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
+        int n = 4;   
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print(" ");
+            }
+
+            System.out.print("*");
+
+            if (i > 1) {
+                for (int j = 1; j <= 2 * i - 3; j++) {
+                    System.out.print(" ");
+                }
+                System.out.print("*");
+            }
+
+            System.out.println();
+        }
+
+        for (int i = n - 1; i >= 1; i--) {
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print(" ");
+            }
+
+            System.out.print("*");
+
+            if (i > 1) {
+                for (int j = 1; j <= 2 * i - 3; j++) {
+                    System.out.print(" ");
+                }
+                System.out.print("*");
+            }
+
+            System.out.println();
+        }
+    }
+}
+        
 
         // TODO: Print a hollow diamond pattern of half-height n using '*'
         //
@@ -20,6 +57,3 @@ public class Main {
         //  *   *
         //   * *
         //    *
-
-    }
-}
